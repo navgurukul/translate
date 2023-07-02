40e2e903b4909dd9e73db0055f31d935d7bcab2b
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2023-07-02 18:23:20 +0530

    first vrsion

---
+++ README.md
@@ -0,0 +1,59 @@
+Certainly! Here's a template for the README file in Markdown format:
+
+```markdown
+# Project Name
+
+Using AI to Universalize Buddha's Teachings
+
+## Description
+
+This project aims to leverage artificial intelligence (AI) to universalize the teachings of Buddha beyond language barriers. By applying AI-based translation techniques to spiritual texts, the project seeks to make the wisdom and teachings accessible to a broader audience worldwide.
+
+For a detailed project description, please refer to the [project description document](https://navgurukul.notion.site/Using-AI-to-universalize-Buddha-s-teachings-beyond-bf09169955e94213b06809c11848952e?pvs=4).
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/your-repository.git
+   ```
+
+2. Install the required dependencies:
+
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+3. Set up the environment:
+
+   - Create a `.env` file in the root directory of the project.
+   - Add the necessary environment variables, such as API keys and file paths, to the `.env` file.
+
+## Usage
+
+1. Place your input Word document in the specified input directory.
+
+2. Run the script:
+
+   ```bash
+   python script.py --input-file path/to/input.docx --output-file path/to/output.docx
+   ```
+
+   Replace `path/to/input.docx` and `path/to/output.docx` with the actual file paths.
+
+3. The translated output will be saved to the specified output file.
+
+## Contributing
+
+Contributions to this project are welcome. To contribute, please follow these steps:
+
+1. Fork the repository.
+
+2. Create a new branch for your feature or bug fix.
+
+3. Make your changes and commit them.
+
+4. Push the changes to your forked repository.
+
+5. Open a pull request in the main repository.
---
+++ script.py
@@ -0,0 +1,112 @@
+import sys
+import openai
+from docx import Document
+from dotenv import dotenv_values
+import csv
+
+# Load the environment variables from the .env file
+env_vars = dotenv_values('.env')
+openai.api_key = env_vars.get('OPENAI_API_KEY')
+replacements_file = 'pre-phrases.csv'
+
+def read_secret_key():
+    # Load the environment variables from the .env file
+    env_vars = dotenv_values('.env')
+
+    # Access the SECRET_KEY variable
+    secret_key = env_vars.get('SECRET_KEY')
+
+    return secret_key
+
+with open(replacements_file, 'r', encoding='utf-8') as csvfile:
+    reader = csv.reader(csvfile)
+    replacements = {row[0]: row[1] for row in reader}
+
+def replace_phrases(text):
+    for phrase, replacement in replacements.items():
+        text = text.replace(phrase, replacement)
+
+    return text
+
+def translate_paragraph(paragraph, target_language='hindi'):
+    # Prepare the system message
+    system_message = {
+        "role": "system",
+        "content": "You are an expert translator specializing in spiritual books. Break longer sentences into shorter ones if needed to make it more readable but keep the meaning and key words intact. Provide the best translation to " + target_language + "."
+    }
+
+    # Prepare the user message with the paragraph to translate
+    user_message = {
+        "role": "user",
+        "content": paragraph
+    }
+
+    # Generate translation using ChatGPT
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[system_message, user_message],
+        max_tokens=3900,
+        temperature=0.7,
+        n=1,
+        stop=None,
+    )
+
+    # Retrieve the translated text from the API response
+    translated_text = response.choices[0].text.strip()
+
+    print(translated_text)
+
+    return translated_text
+
+def output_translation(input_path, output_path, target_language='en'):
+    # Load the input document
+    doc = Document(input_path)
+
+    # Create a new document for the output
+    output_doc = Document()
+
+    # Iterate over paragraphs in the input document
+    for paragraph in doc.paragraphs:
+        # Replace phrases in the paragraph before translation
+        replaced_paragraph = replace_phrases(paragraph.text)
+
+        # Break down the paragraph into sub-paragraphs if necessary
+        sub_paragraphs = []
+        if len(replaced_paragraph) > 2000:
+            sub_paragraphs = [replaced_paragraph[i:i+2000] for i in range(0, len(replaced_paragraph), 2000)]
+        else:
+            sub_paragraphs.append(replaced_paragraph)
+
+        # Translate each sub-paragraph
+        translated_sub_paragraphs = []
+        for sub_paragraph in sub_paragraphs:
+            translated_sub_paragraph = translate_paragraph(sub_paragraph, target_language)
+            translated_sub_paragraphs.append(translated_sub_paragraph)
+
+        # Add the original and translated sub-paragraphs to the output document
+        for i in range(len(sub_paragraphs)):
+            # Add the original sub-paragraph to the output document
+            output_doc.add_paragraph(sub_paragraphs[i])
+            output_doc.add_paragraph(sub_paragraphs[i])
+
+            # Add the translated sub-paragraph to the output document
+            output_doc.add_paragraph(translated_sub_paragraphs[i])
+
+    # Save the output document
+    output_doc.save(output_path)
+
+if __name__ == '__main__':
+    # Check if the correct number of command line arguments is provided
+    if len(sys.argv) < 3:
+        print("Usage: python script.py input_file output_file [target_language]")
+        sys.exit(1)
+
+    # Extract the input and output file names from command line arguments
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    # Extract the target language if provided, default to 'en' if not specified
+    target_language = sys.argv[3] if len(sys.argv) > 3 else 'hindi'
+
+    # Call the function to output the translation
+    output_translation(input_file, output_file, target_language)
