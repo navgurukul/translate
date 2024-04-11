5df65bfe6cc1a13ee302c41125080c204027de11
Abhishek Gupta <abhishekgupta.iitd@gmail.com>
2024-04-11 19:50:37 +0530

    gpt + google

--- translation.py
+++ translation.py
@@ -1,14 +1,15 @@
 import html
 # Imports the Google Cloud Translation library
+import openai
 from google.cloud import translate
 from get_lang import pali_transform
 import os
 import csv
 from dotenv import dotenv_values
 
-# Load the environment variables from the .env file
 env_vars = dotenv_values('.env')
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = env_vars.get('GOOGLE_APPLICATION_CREDENTIALS')
+openai_key = env_vars.get('OPENAI_API_KEY')
 
 pre_replacements_file = 'pre-phrases'
 post_replacements_file = 'post-phrases.csv'
@@ -57,6 +58,13 @@ def translate_text_wrapper(text, source_language='en',target_language='hi', mode
 
     client = translate.TranslationServiceClient()
 
+    if target_language=='hi':
+        lang_full = 'Hindi'
+    elif target_language=='ta':
+        lang_full = 'Tamil'
+    else:
+        raise ValueError("Invalid target language")
+
     if model=='google':
         if target_language=='hi':
             # This one is a custom trained model in Hindi and wouldn't work for other languages.
@@ -89,11 +97,39 @@ def translate_text_wrapper(text, source_language='en',target_language='hi', mode
                         "target_language_code": target_language,
                     }
                 )
+
+        translated_text = response.translations[0].translated_text
+
+    elif model=='gpt':
+            
+        # Set up the OpenAI API client
+        openai.api_key = openai_key
+
+        # Define the prompt for the GPT-3.5 model
+        prompt = f"""Translate this in {lang_full}. Use language appropriate to what is used to deliver or explain Buddha's teachings \n\n {text.replace('<span class="notranslate">','').replace('</span>','')}"""
+
+        # Generate the response using the GPT-3.5 model
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are a translator for a book on Vipassana or Buddha's teachings."},
+                {"role": "user", "content": prompt},
+            ],
+            max_tokens=1000,
+            n=1,
+            stop=None,
+            temperature=0.1,
+        )
+
+        print(response.choices[0].message.content)
+
+        # Extract the generated response from the API response
+        translated_text = response.choices[0].message.content.strip()
+
     else:
         print("Model not supported:", model)
         return
 
-    translated_text = response.translations[0].translated_text
     # translated_text = "Translated String: " + text
     translated_text = post_replace_phrases(translated_text, source_language, target_language).replace('\n','').replace('<span class="notranslate">','').replace('</span>','')
     # replace all occurences of ascii characters like &#39 to the corresponding character
